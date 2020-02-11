aa4f429c18c0ef7edffb2252a4a7c300b9c85ed8
LisbethC11 <60081288+LisbethC11@users.noreply.github.com>
2020-02-11 06:20:48 +0100

    commit

--- app/src/main/java/com/example/headsupdisplaygame/splash.java
+++ app/src/main/java/com/example/headsupdisplaygame/splash.java
@@ -2,13 +2,32 @@ package com.example.headsupdisplaygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class splash extends AppCompatActivity {
+    private final int Duracion_Splash = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            ;
+        }, Duracion_Splash);
+
     }
 }
