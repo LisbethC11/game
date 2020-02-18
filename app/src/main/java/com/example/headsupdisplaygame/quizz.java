affa4b96fa27870c82e3e7c8f52d62f57b548e54
3DWIN13 <ecolladocastillo@gmail.com>
2020-02-17 20:01:07 -0400

    se puede boltiar la pantalla

--- app/src/main/java/com/example/headsupdisplaygame/quizz.java
+++ app/src/main/java/com/example/headsupdisplaygame/quizz.java
@@ -2,6 +2,7 @@ package com.example.headsupdisplaygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class quizz extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class quizz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
