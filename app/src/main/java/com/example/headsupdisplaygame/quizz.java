0486b4f56d2beab49bf643de0fb58fd208d486e3
3DWIN13 <ecolladocastillo@gmail.com>
2020-02-18 00:47:49 -0400

    akgo

--- app/src/main/java/com/example/headsupdisplaygame/quizz.java
+++ app/src/main/java/com/example/headsupdisplaygame/quizz.java
@@ -4,14 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+
+
+
+import java.util.ArrayList;
+
 
 public class quizz extends AppCompatActivity {
 
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    
+
+        EmpezarJuego();
+    
+    }
+
+    private void EmpezarJuego() {
+
+
     }
 }
